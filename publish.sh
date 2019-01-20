ddf97cd63c3443d82690f064d0611faa12310993
J.J. Gomez-Cadenas <jjgomezcadenas@gmail.com>
2019-01-20 20:15:42 +0100

    delete index.html

--- publish.sh
+++ publish.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 rm index.ipynb
-rm index.html
 rm -rf img
 cp ../sabatnb/prlSMFI.ipynb index.ipynb
 cp -r ../sabatnb/img img
 jupyter-nbconvert --to slides index.ipynb --reveal-prefix=reveal.js
-mv index.slides.html  index.html
+cp index.slides.html  index.html
 
