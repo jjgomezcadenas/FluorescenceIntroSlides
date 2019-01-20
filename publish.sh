7da25b71de2e7fe0ec4c9855cab424ac34b4308c
J.J. Gomez-Cadenas <jjgomezcadenas@gmail.com>
2019-01-20 16:34:33 +0100

    Updating slides

--- publish.sh
+++ publish.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-cp prlSMFI.ipynb index.ipynb
+rm index.ipynb
+rm index.html
+cp ../sabatnb/prlSMFI.ipynb index.ipynb
 jupyter-nbconvert --to slides index.ipynb --reveal-prefix=reveal.js
 mv index.slides.html  index.html
 
