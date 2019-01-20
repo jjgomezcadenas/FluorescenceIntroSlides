3f0f9532b22857d93f0fcbe55ccfb11ea199d34a
J.J. Gomez-Cadenas <jjgomezcadenas@gmail.com>
2019-01-20 20:00:49 +0100

    Adding content

--- publish.sh
+++ publish.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 rm index.ipynb
 rm index.html
+rm -rf img
 cp ../sabatnb/prlSMFI.ipynb index.ipynb
+cp -r ../sabatnb/img img
 jupyter-nbconvert --to slides index.ipynb --reveal-prefix=reveal.js
 mv index.slides.html  index.html
 
